a717aaeb7d626ad1155248fab0c3db950a7c2ef2
Aswin-S-T <aswinswebart15@gmail.com>
2023-06-13 13:42:36 +0530

    bottom navigation done

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen";
+import SettingScreen from "./screens/SettingScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons name="md-home" size={25} color={"darkcyan"} />
+        ),
+      },
+    },
+    User: {
+      screen: UserScreen,
+      navigationOptions: {
+        tabBarLabel: "User",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-person-circle-outline"
+            size={25}
+            color={"darkcyan"}
+          />
+        ),
+      },
+    },
+    Setting: {
+      screen: SettingScreen,
+      navigationOptions: {
+        tabBarLabel: "Setting",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons name="md-settings-outline" size={25} color={"darkcyan"} />
+        ),
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarLabel: "User",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-person-circle-outline"
+            size={25}
+            color={"darkcyan"}
+          />
+        ),
+      },
+    },
+  },
+
+  {
+    initialRouteName: "Home",
+    barStyle: { backgroundColor: "#fff", color: "darycyan", padding: 8 },
+  }
+);
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigator>
+      <HomeScreen />
+    </Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
